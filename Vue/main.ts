1b5517370cae83ddefd43a317c48972e6b059406
Pradeep <webreinvent@gmail.com>
2022-06-24 20:31:07 +0530

    added: oruga to the project

--- Vue/main.ts
+++ Vue/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Oruga from '@oruga-ui/oruga-next'
+import { bulmaConfig } from '@oruga-ui/theme-bulma'
 
 import Default from './layouts/Default.vue'
 import router from './router'
 
-const app = createApp(Default)
+const app = createApp(Default);
 
+app.use(Oruga, bulmaConfig);
 app.use(createPinia())
 app.use(router)
 
--- Vue/pages/dashboard/Dashboard.vue
+++ Vue/pages/dashboard/Dashboard.vue
@@ -3,6 +3,12 @@
         <h1 v-if="heading" class="title">
             {{heading}}
         </h1>
+
+        <section>
+            <o-button variant="primary" >Click Me</o-button>
+            <o-button variant="warning" >Click Me</o-button>
+        </section>
+
     </div>
 </template>
 
